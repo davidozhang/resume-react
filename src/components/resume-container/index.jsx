21c30939080fe11e45f842f736bab2eca47512d5
zuqini <lizuqiliang@gmail.com>
2016-07-27 02:04:10 -0700

    added main body layout

--- src/components/resume-container/index.jsx
+++ src/components/resume-container/index.jsx
@@ -4,11 +4,15 @@ const cssModules = require('react-css-modules');
 const styles = require('./styles.scss');
 
 const Header = require('../header/index.jsx');
+const MainSection = require('../main-section/index.jsx');
+const SideSection = require('../side-section/index.jsx');
 
 const ResumeContainer = (props) => {
     return (
         <div styleName='resume-container'>
             <Header term='3B' />
+            <MainSection />
+            <SideSection />
         </div>
     );
 };
